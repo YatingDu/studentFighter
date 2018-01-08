464ac7b4c25c7c369bcddadcd0c773e291921b0d
dinawu <2426647428@qq.com>
2018-01-08 15:51:12 +0800

    first

---
+++ student_fighter_part1-1.py
@@ -0,0 +1,10 @@
+class StudentFighter(object):
+	def _init_(self, strength, health, name):
+		  self.strength = strength
+        self.name = name
+        self.health = health
+
+kalu = StudentFighter(strength=3, health=100, name="Kalu")
+david = StudentFighter(strenghth=5, health=100, name="David")
+
+print()
